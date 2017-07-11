513e596f2c4fee3cbaa5dc1980d575cb6ee5031d
Ivanov Vladimir <ivanov@k2it.ru>
2017-07-12 00:51:07 +0300

    react ui component added

--- src/webparts/gameWebPart/components/GameWebPart.tsx
+++ src/webparts/gameWebPart/components/GameWebPart.tsx
@@ -3,23 +3,41 @@ import styles from './GameWebPart.module.scss';
 import { IGameWebPartProps } from './IGameWebPartProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
+import {
+    DocumentCard,
+    DocumentCardPreview,
+    DocumentCardTitle,
+    DocumentCardActivity,
+    IDocumentCardPreviewProps
+} from 'office-ui-fabric-react/lib/DocumentCard';
+
 export default class GameWebPart extends React.Component<IGameWebPartProps, void> {
-  public render(): React.ReactElement<IGameWebPartProps> {
+  public render(): JSX.Element {
+    const previewProps: IDocumentCardPreviewProps = {
+      previewImages: [
+        {
+          previewImageSrc: String(require('./document-preview.png')),
+          iconSrc: String(require('./icon-ppt.png')),
+          width: 318,
+          height: 196,
+          accentColor: '#ce4b1f'
+        }
+      ],
+    };
+
     return (
-      <div className={styles.helloWorld}>
-        <div className={styles.container}>
-          <div className={`ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`}>
-            <div className="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
-              <span className="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
-              <p className="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
-              <p className="ms-font-l ms-fontColor-white">{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={styles.button}>
-                <span className={styles.label}>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DocumentCard onClickHref='http://bing.com'>
+        <DocumentCardPreview { ...previewProps } />
+        <DocumentCardTitle title='Revenue stream proposal fiscal year 2016 version02.pptx' />
+        <DocumentCardActivity
+          activity='Created Feb 23, 2016'
+          people={
+            [
+              { name: 'Kat Larrson', profileImageSrc: String(require('./avatar-kat.png')) }
+            ]
+          }
+        />
+      </DocumentCard>
     );
   }
 }
